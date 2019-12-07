cf291a39611aed24fb4f92b689f34c338c1d0cd7
lixun <againstnow@gmail.com>
2019-12-07 20:05:30 +0800

    添加一些额外的文件

--- temp/1.python简介.py
+++ temp/1.python简介.py
@@ -1,1 +1,31 @@
-# -*-coding:utf-8-*-import platformprint "start learn python"# declare a variablei = 10print i+100print type(i)name = "hello"print name,"the type is", type(name)print platform.uname()print "你好"
+#
+# -*-coding:utf-8-*-
+
+import platform
+
+print "start learn python"
+
+# declare a variable
+
+i = 10
+print i+100
+print type(i)
+
+name = "hello"
+print name,"the type is", type(name)
+
+print platform.uname()[1]
+print platform.machine()
+print "你好"
+print "hello"
+
+import os
+print os.getcwd()
+os.chdir('D:\Python')
+print os.getcwd()
+os.mkdir("hello")
+os.rmdir("hello")
+print os.abort()
+
+if __name__ == '__main__':
+    pass
---
+++ temp/3.Python常见用法.py
@@ -0,0 +1,11 @@
+﻿#coding=utf-8
+#/data1/Python2.7/bin/python2.7
+# author: Lixun
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+    
