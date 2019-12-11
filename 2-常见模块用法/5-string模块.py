a4436fe9ed0a3e255ac032cedf3f58fe9f919029
lixun <againstnow@gmail.com>
2019-12-11 21:58:04 +0800

    完善string，添加re模块

--- 2-常见模块用法/5-string模块.py
+++ 2-常见模块用法/5-string模块.py
@@ -32,6 +32,112 @@ s = "The quick brown fox ro juromped over the lazy dog."
 sub = "ro"
 print s.find(sub)
 
+# index(s):用于查找字符串，如果不存在返回异常
+
+s = "The quick brown fox ro juromped over the lazy dog."
+sub = "roo"
+try:
+    print s.index(sub)
+except:
+    print "throw error"
+
+# True：至少有一个字符并且都是字母或者数字
+s = "The quick brown fox ro juromp1ed over the lazy dog."
+s1 = ""
+print s.isalnum(),s1.isalnum()
+
+# True:至少存在一个字符，并且都是字母
+s = "The quick br4ow2n1 fo3x ro ju5romp1ed o3ver th4e lazy dog."
+s1 = "Thequick"
+print s.isalpha(),s1.isalpha()
+
+# True: 只包含数字
+s = "111111 223"
+s1 = "11111"
+print s.isdigit(),s1.isdigit()
+
+# True: 只包含空格
+s = "    "
+s1 = " 1 2 3"
+print s.isspace(),s1.isspace()
+
+# ljust():左对齐，使用空格从左边开始填充
+s = "                        Actions speak louder than words"
+print s.ljust(56)
+
+# .lstrip():去掉左边开始的不可见字符
+s = "                        Actions speak louder than words"
+print s.lstrip()
+
+# S.partition(s):用 S 将 str 切分成为三个值
+sf = "From small beginnings comes great things"
+ss = "small"
+print sf.partition(ss)
+
+
+# lower():用于将大写转换称为小写
+s = "Every man is his own worst enemy"
+print s.lower()
+
+# a.replace(b): 使用 b 替换 a 里面的字符串
+s = "The voice of one man is the voice of no one"
+sub = "two"
+print s.replace("one",sub)
+
+# str.rfind():用于从右边开始进行查找
+s = "The voice of one man is the voice of no one"
+sub = "one"
+print s.rfind(sub),s.find(sub)
+
+# str.rindex():类似于index，从右边开始查找
+s = "The voice of one man is the voice of no one"
+sub = "one"
+print s.index(sub),s.rindex(sub)
+
+# str.rjust():返回一个右对齐的，使用空格填充至长度为width的新字符串
+s = "The voice of one man is the voice of no one"
+print s.rjust(56)
+
+# str.rpartition(s): 类似于partition，从右边开始查找
+sf = "From small beginnings comes small great things"
+ss = "small"
+print sf.rpartition(ss)
+
+# r.strip():去掉右边的不可见字符
+s = "    Actions speak louder than words             "
+print s.rstrip()
+
+# s.split(s):以s为分隔符号进行切片
+s = "A great ship asks for deep waters Two heads are better than one " * 4
+print s.split("A")[0]
+print s.split("are")[2]
+print type(s.split("A"))
+
+# s.splitlines(): 按照行进行分割，返回一个包含各行元素的列表，用于处理文件比较常见
+
+# s.startwith(s):判断字符串S是否是按照s开头的
+s = "A great ship asks for deep waters Two heads are better than one " * 4
+print s.startswith("A"),s.startswith("G")
+
+# s.strip():相当于同时执行 lstrip(), rstrip()
+
+s = "                        Actions speak louder than words                   "
+print s.lstrip()
+
+# s.title():返回所有单次首字母大写的，其余为小写
+s = "If a thing is worth doing it is worth worth doing well."
+print s.title()
+
+# str.upper():返回str所有字符的大写字符串
+s = "If a thing is worth doing it is worth worth doing well."
+print s.upper()
+
+# str.zfill(width): 返回长度为 width 的字符串，原字符str右对齐，填充 0
+s = "If a thing is worth doing it is worth worth doing well."
+s.zfill(178)
+
+#
+
 # 用于进行字符转换
 # 首先需要定义字符转换规则
 leet = string.maketrans('abegiloprstz', '463611092572')
@@ -39,4 +145,44 @@ s = "The quick brown fox jumped over the lazy dog."
 print s
 print string.translate(s,leet)
 
+# 字符串常用技巧
+# 字符串反转
+str = "123456"
+print str, str[::-1]
+
+# 字符串拼接尽量使用join,+需要多次申请内存,按照以下方式拼接字符串
+s = "Nothing "
+s1 = "great "
+s2 = "was "
+s3 = "ever "
+s4 = "achieved "
+s5 = "without "
+s6 = "enthusiasm."
+print ''.join([s,s1,s2,s3,s4,s5,s6])
+print s + s1 + s2 + s3 + s4 + s5 + s6
+
+# 按照固定长度分割字符串
+import re
+s = "1234567890"
+print re.findall(r'.{1,3}',s)
+
+# 优雅的字符串
+SQL = ( 'SELECT COUNT(*) FROM TABLE_NAME '
+        'WHER ID = 10 '
+        'GROUP BY SEX'
+)
+print SQL
+
+# 将print 的字符串写入文件
+print >> open("somefile.txt", "w+"), "Hello World"
+
 #
+
+
+
+
+
+
+
+
+
---
+++ 2-常见模块用法/6-re模块.py
@@ -0,0 +1,81 @@
+# -*-coding:utf-8-*-
+#!/data1/Python2.7/bin/python2.7
+# author: Lixun
+# re 模块：主要用于进行正则表达式匹配
+#
+
+import re
+# re.match():如果匹配到返回一个match实例对象，否则返回None,math只匹配开头
+a = 'abcdefg'
+print re.match(r'abc',a)
+print re.match(r'abc',a).group()
+# 对于匹配失败的返回NULL
+print re.match(r'cba',a)
+
+# re.search(): 用于查找整个字符串中可以匹配到子串，找到返回对应的实例，否则返回NULL,
+#
+a  = 'abcdefg'
+print re.search(r'bc',a)
+print re.search(r'bc',a).group()
+print re.search(r'123',a)
+
+# sub：用于进行替换，并且返回替换的次数
+a = 'a1b2c3'
+print re.sub(r'\d+','0',a),type(re.sub(r'\d+','0',a)) # 在a里面把数字替换成为0
+
+# re.split(): 用pattern匹配的子串来分割string，如果pattern里使用了圆括号，那么被pattern匹配到的串也将作为返回值列表的一部分,maxsplit为最多被分割的字符串个数
+a = 'a4b5c7'
+print re.split(r'\d',a)
+print re.split(r'(\d)',a)
+
+# re.findall(): 通过列表的方式返回string里面pattern匹配的子串
+a = "das12das3cds4fds5fadas22fs6"
+print re.findall(r'\d',a)
+print re.findall(r'\d{2,}',a)
+
+# match 对象
+# re.match()、re.search()成功匹配的话都会返回一个Match对象，它包含了关于此次匹配的信息，可以使用Match提供的属性或方法来获取这些信息
+# (?P<sign>.*) 这个没有看懂
+
+m = re.match(r'(\w+) (\w+)(?P<sign>.*)', 'hello world!')
+print "m.string:", m.string
+print "m.re:", m.re
+print "m.pos:", m.pos
+print "m.endpos:", m.endpos
+print "m.lastindex:", m.lastindex
+print "m.lastgroup:", m.lastgroup
+print "m.group(1,2):", m.group(1, 2)
+print "m.groups():", m.groups()
+print "m.groupdict():", m.groupdict()
+print "m.start(2):", m.start(2)
+print "m.end(2):", m.end(2)
+print "m.span(2):", m.span(2)
+print r"m.expand(r'\2 \1\3'):", m.expand(r'\2 \1\3')
+
+# Pattern对象
+# 通过re.complie()生成Pattern对象
+pa = re.compile("(\d)")
+print pa.split("he has 2 books and 1 pen")
+print pa.findall("he has 2 books and 1 pen")
+print pa.sub('much', 'he has 2 books and 1 pen')
+
+# 匹配模式
+# 匹配模式取值可以使用按位或运算符'|'表示同时生效
+# 常见的匹配模式
+# re.IGNORECASE 表示忽略大小写
+pa = re.compile('abc',re.IGNORECASE)
+print pa.findall("AbCdefg")
+
+# re.LOCALE：表示字符集本地化
+
+#
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+    
