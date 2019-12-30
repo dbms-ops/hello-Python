154e112f72b95ded8c89df3f624e4fdac544a61a
lixun <againstnow@gmail.com>
2019-12-30 23:47:48 +0800

    little change

--- 7-面向对象/5-保护类的私有属性.py
+++ 7-面向对象/5-保护类的私有属性.py
@@ -4,6 +4,7 @@
 # 类的某些私有属性是不应该在类的外面被访问的
 # 
 
+
 class Person(object):
     # 定义属性：本质上就是在定义变量,这里的变量是以下的方法所公用的，特例在具体的方法中声明
 
@@ -13,20 +14,23 @@ class Person(object):
         self.weight = weight
         self.height = height
         # 类的私有属性，例如money是不应该在类的外面被访问的,通过变量名前加上__来实现,
-        self.__money = money # 重命名为self._Person__money
+        self.__money = money  # 重命名为self._Person__money
 
         print "You can't see me,add"
         print self.__money
+
     def money(self):
         print self.__money
 
     # 通过添加内部的方法来使用内部的属性
     def getmoney(self):
         print self.__money
-    def setmoney(self,money):
+
+    def setmoney(self, money):
         if self.__money < money:
             self.__money = money
         print self.__money
+
 
 tom = Person("tom", 11, 120, 30, 11000)
 # 这种方式类似于添加了一个名为__money的属性，并不是在访问类里面的__money属性
@@ -43,6 +47,7 @@ tom._Person__money = 10
 print tom._Person__money
 
 # 在Python中，还存在 _变量，按照约定的规则，表示虽然可以被访问，但是不建议这么做，请把这个变量当做私有变量，不要直接访问
+#
 
 if __name__ == '__main__':
     pass
