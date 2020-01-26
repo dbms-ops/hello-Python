5257f6017ead7aba2ffbeb80a88a077c88972691
lixun <againstnow@gmail.com>
2020-01-26 20:58:30 +0800

    fix bugs

--- 2-常见模块用法/5-正则表达式/2-glob模块.py
+++ 2-常见模块用法/5-正则表达式/2-glob模块.py
@@ -3,13 +3,21 @@
 # !/data1/Python2.7/bin/python27
 # time: 2020-01-23 22:43 
 # user: linux
-# description: glob 模块用于实现正则表达式的匹配，实现全局的模糊查找,
-#
+# description: glob 模块用于实现正则表达式的匹配，实现全局的模糊查找,支持的正则表达式匹配，这里的正则匹配规则和标准的正则表达式不一样；
+# 支持的通配符：
+#   *：匹配0或多个字符；
+#   **：匹配所有文件、目录、子目录和子目录里的文件 3.5版本新增；
+#   ?：匹配1个字符，与正则表达式里的?不同；
+#   [exp]：匹配指定范围内的字符，如：[1-9]匹配1至9范围内的字符；
+#   [!exp]：匹配不在指定范围内的字符；
+# 脚本默认是通过os.chdir()当前目录查找；
+
+
 import glob
 import os
 
 
-def find_file(file_path='/etc/snmp/'):
+def find_glob(file_path='/etc/snmp/'):
     if os.path.exists(file_path):
         os.chdir(file_path)
         for file_needs in glob.glob(''):
@@ -18,7 +26,7 @@ def find_file(file_path='/etc/snmp/'):
         print "path: {} is not exists".format(file_path)
 
 
-def find_file(file_path):
+def find_iglob(file_path):
     if os.path.exists(file_path):
         os.chdir(file_path)
         for file_needs in glob.iglob(''):
@@ -28,7 +36,8 @@ def find_file(file_path):
 
 
 def main():
-    find_file()
+    find_glob()
+
 
 
 if __name__ == "__main__":
--- 2-常见模块用法/5-正则表达式/3-fnmatch模块.py
+++ 2-常见模块用法/5-正则表达式/3-fnmatch模块.py
@@ -4,11 +4,55 @@
 # time: 2020-01-24 10:21 
 # user: linux
 # description: fnmatch 模块用于实现
-# 
+# 支持的通配符：
+#   *：匹配0或多个字符；
+#   **：匹配所有文件、目录、子目录和子目录里的文件 3.5版本新增；
+#   ?：匹配1个字符，与正则表达式里的?不同；
+#   [exp]：匹配指定范围内的字符，如：[1-9]匹配1至9范围内的字符；
+#   [!exp]：匹配不在指定范围内的字符；
+
+
+import fnmatch
+import os
+
+
+def fnmatch_fnmatch(file_path, pattern):
+    """
+    Patterns are Unix shell style:
+
+    *       matches everything
+    ?       matches any single character
+    [seq]   matches any character in seq
+    [!seq]  matches any char not in seq
+
+    An initial period in FILENAME is not special.
+    Both FILENAME and PATTERN are first case-normalized
+    if the operating system requires it.
+    If you don't want this, use fnmatchcase(FILENAME, PATTERN).
+    :param file_path:
+    :param pattern:
+    :return:
+    """
+    for file_name in os.listdir(file_path):
+        result = fnmatch.fnmatch(file_name, pattern)
+        print result
+
+
+def fnmatch_filter(file_path, file_pattern):
+    """
+    Return the subset of the list NAMES that match PAT
+    :param file_path:
+    :param file_pattern:
+    :return:
+    """
+    for file_name in os.listdir(file_path):
+        return fnmatch.filter(file_name, file_pattern)
+
+
 
 
 def main():
-    pass
+    print fnmatch_fnmatch('/tmp/', 'pycharm_*')
 
 
 if __name__ == "__main__":
